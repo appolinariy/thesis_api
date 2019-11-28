7d79529b3d63c5f9910e5cd42529b75edeac5fac
appolinariy <abramova.polina.2001@mail.ru>
2019-11-29 00:39:40 +0300

    fixed bags

--- queries.js
+++ queries.js
@@ -12,9 +12,9 @@ const pool = new Pool({
 const getFilials = (request, response) => {
     pool.query('select * from filial order by id_filial ASC', (error, results) => {
         if(error) {
-            response.status(500).send({message: 'Something went wrong'});
+            response.status(500).send({message: 'Something went wrong', status: false});
         }
-        response.status(200).json(results.rows)
+        response.status(200).send({data: results.rows, status: true})
     })
 }
 
@@ -23,9 +23,9 @@ const getBankUserById = (request, response) => {
     const login = request.params.login
     pool.query('select surname, name, father_name, position, login, system_role, filial.address from bank_user,filial where bank_user.id_filial=filial.id_filial and login = $1;', [login], (error, results) => {
         if (error) {
-            response.status(500).send({message: 'Something went wrong'});
+            response.status(500).send({message: 'Something went wrong', status: false});
         }
-        response.status(200).json(results.rows)
+        response.status(200).send({data: results.rows, status: true})
     })
 }
 
@@ -33,9 +33,9 @@ const getBankUserById = (request, response) => {
 const getAllBankUser = (request, response) => {
     pool.query('select * from bank_user order by id_user ASC;', (error, results) => {
         if (error) {
-            response.status(500).send({message: 'Something went wrong'});
+            response.status(500).send({message: 'Something went wrong', status: false});
         }
-        response.status(200).json(results.rows)
+        response.status(200).send({data: results.rows, status: true})
     })
 }
 
@@ -45,10 +45,10 @@ const createBankUser = (request, response) => {
     //(select id_filial from ... where address = $7) in values(...)
     pool.query('insert into bank_user (surname, name, father_name, position, login, password, id_filial, system_role) values ($1, $2, $3, $4, $5, $6, $7, $8) returning id_user', [surname, name, father_name, position, login, password, id_filial, system_role], (error, results) => {
         if (error) {
-            response.status(400).send({message: 'Empty fields'});
-            response.status(500).send({message: 'Something went wrong'});
+            response.status(400).send({message: 'Empty fields', status: false});
+            response.status(500).send({message: 'Something went wrong', status: false});
         }
-        response.status(201).send({message: `User added with ID: ${results.rows[0].id_user}`, id_user: results.rows[0].id_user})
+        response.status(201).send({message: `User added with ID: ${results.rows[0].id_user}`, id_user: results.rows[0].id_user, status: true})
     })
 }
 
@@ -58,9 +58,9 @@ const deleteBankUser = (request, response) => {
     console.log(id_user);
     pool.query('delete from bank_user where id_user = $1', [id_user], (error, results) => {
         if(error) {
-            response.status(500).send({message: 'Something went wrong'});
+            response.status(500).send({message: 'Something went wrong', status: false});
         }
-        response.status(200).send(`User deleted with ID: ${id_user}`)
+        response.status(200).send({message: `User deleted with ID: ${id_user}`, status: true})
     })
 }
 
@@ -68,17 +68,20 @@ const deleteBankUser = (request, response) => {
 const updateBankUser = (request, response) => {
     const id_user = parseInt(request.params.id_user)
     const { surname, name, father_name, position, login, password, id_filial, system_role } = request.body
+    if(id_filial && login && id_user) {
     pool.query(
         'update bank_user set surname = $1, name = $2, father_name = $3, position = $4, login = $5, password = $6, id_filial = $7, system_role = $8 where id_user = $9',
         [surname, name, father_name, position, login, password, id_filial, system_role, id_user],
         (error, results) => {
             if (error) {
-                response.status(400).send({message: 'Empty fields'});
-                response.status(500).send({message: 'Something went wrong'});
+                response.status(500).send({message: 'Something went wrong', status: false});
             }
-            response.status(200).send(`User modified with ID: ${id_user}`)
+            response.status(200).send({message: `User modified with ID: ${id_user}`, status: true})
         }
     )
+    } else {
+        response.status(400).send({message: 'Empty fields', status: false});
+    }
 }
 
 //export - to access these functions from index.js
