103a49830384f96d42eb1a725ef37a57cd17912c
appolinariy <abramova.polina.2001@mail.ru>
2020-02-28 23:15:56 +0300

    add getting fields to auth

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3001;
 
-const queries = require('./queries');
+const queries = require("./queries");
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
-)
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");// update to match the domain you will make the request from
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
 });
 
-app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
 
 //Auth
-app.post('/auth', queries.authorization);
+app.post("/auth", queries.authorization);
 
 //Filial
-app.get('/filials', queries.getFilials);
+app.get("/filials", queries.getFilials);
 
 //Client
-app.get('/allclients', queries.getAllClients);
-app.post('/allclients', queries.createClient);
-app.put('/allclients/:id_client', queries.updateClient);
-app.delete('/allclients/:id_client', queries.deleteClient);
-app.get('/findclients/:surname', queries.findClient);
+app.get("/allclients", queries.getAllClients);
+app.post("/allclients", queries.createClient);
+app.put("/allclients/:id_client", queries.updateClient);
+app.delete("/allclients/:id_client", queries.deleteClient);
+app.get("/findclients/:surname", queries.findClient);
 
 //Administration
-app.get('/bankuser/:id_user', queries.getBankUserById);
-app.get('/allbankusers', queries.getAllBankUser);
-app.post('/allbankusers', queries.createBankUser);
-app.delete('/allbankusers/:id_user', queries.deleteBankUser);
-app.put('/allbankusers/:id_user', queries.updateBankUser);
-app.get('/findbankuser/:surname', queries.findBankUser);
+app.get("/bankuser/:id_user", queries.getBankUserById);
+app.get("/allbankusers", queries.getAllBankUser);
+app.post("/allbankusers", queries.createBankUser);
+app.delete("/allbankusers/:id_user", queries.deleteBankUser);
+app.put("/allbankusers/:id_user", queries.updateBankUser);
+app.get("/findbankuser/:surname", queries.findBankUser);
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
+  console.log(`App running on port ${port}.`);
 });
--- queries/filial.js
+++ queries/filial.js
@@ -1,15 +1,15 @@
-const db = require('../db');
+const db = require("../db");
 
 const pool = db.pool;
 
 //get all filials by id - Fillials
 const getFilials = (request, response) => {
-    pool.query('select * from filial order by id_filial ASC', (error, results) => {
-        if (error) {
-            response.status(500).send({ message: 'Something went wrong', status: false });
-        }
-        response.status(200).send({ data: results.rows, status: true })
-    })
-}
+  pool.query("select * from filial order by id_filial ASC", (error, results) => {
+    if (error) {
+      response.status(500).send({ message: "Something went wrong", status: false });
+    }
+    response.status(200).send({ data: results.rows, status: true });
+  });
+};
 
 module.exports = { getFilials };
