da372c83693438feb597a6d395fb31d2c0cb2f57
appolinariy <abramova.polina.2001@mail.ru>
2020-03-31 19:27:06 +0300

    added filterContract()

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get("/findclients/:surname", queries.findClient);
 app.get("/allcontracts", queries.getContracts);
 app.post("/allcontracts", queries.createContract);
 app.get("/findcontract/:number_contract", queries.findContract);
-app.get("/filtercontract/:start_date", queries.filterContract);
+app.get("/filtercontract/fromdate/:from_date/todate/:to_date", queries.filterContract);
 
 // Payment
 app.get("/allpayments/:number_contract", queries.getPaymentSchedule);
--- queries/contract.js
+++ queries/contract.js
@@ -68,8 +68,7 @@ const findContract = async (request, response) => {
     let results = await pool.query(
       `select contract.*, client.surname, client.name, client.father_name from contract join client on contract.id_client=client.id_client where number_contract like '%${number_contract}%'order by id_contract ASC;`
     );
-    let borrowers = await pool.query(`select surname, name, father_name, id_client from client;`);
-    response.status(200).send({ data: results.rows, borrowers: borrowers.rows, status: true });
+    response.status(200).send({ data: results.rows, status: true });
   } catch (err) {
     response.status(500).send({ message: "Something went wrong", status: false });
     console.log(err);
@@ -84,8 +83,7 @@ const filterContract = async (request, response) => {
     let results = await pool.query(
       `select contract.*, client.surname, client.name, client.father_name from contract join client on contract.id_client=client.id_client where start_date>='${from_date}' and start_date<='${to_date}';`
     );
-    let borrowers = await pool.query(`select surname, name, father_name, id_client from client;`);
-    response.status(200).send({ data: results.rows, borrowers: borrowers.rows, status: true });
+    response.status(200).send({ data: results.rows, status: true });
   } catch (err) {
     response.status(500).send({ message: "Something went wrong", status: false });
     console.log(err);
